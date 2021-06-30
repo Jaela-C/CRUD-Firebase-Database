bc27e777e5b1f7bce6ddbdd7abdfe7938c803c05
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-06-29 23:58:35 -0500

    CRUD en Database (Review)

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCv_PX8qyBmrXuoZLPGUSxYBpV-3pb3aR0",
+    authDomain: "crud-firebase-database-225ad.firebaseapp.com",
+    databaseURL: "https://crud-firebase-database-225ad-default-rtdb.firebaseio.com",
+    projectId: "crud-firebase-database-225ad",
+    storageBucket: "crud-firebase-database-225ad.appspot.com",
+    messagingSenderId: "771624806842",
+    appId: "1:771624806842:web:e97baa27cca31f6b1c1583",
+    measurementId: "G-0GL0TK22YT"
+  }
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCv_PX8qyBmrXuoZLPGUSxYBpV-3pb3aR0",
+    authDomain: "crud-firebase-database-225ad.firebaseapp.com",
+    databaseURL: "https://crud-firebase-database-225ad-default-rtdb.firebaseio.com",
+    projectId: "crud-firebase-database-225ad",
+    storageBucket: "crud-firebase-database-225ad.appspot.com",
+    messagingSenderId: "771624806842",
+    appId: "1:771624806842:web:e97baa27cca31f6b1c1583",
+    measurementId: "G-0GL0TK22YT"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
